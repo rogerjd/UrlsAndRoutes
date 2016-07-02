21ea006e7b88e9236d36d778a933ce3dc04bddda
roger <rjpd@netzero.com>
2016-07-01 20:33:33 -0400

    Add project files.

---
+++ UrlsAndRoutes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UrlsAndRoutes.Startup))]
+namespace UrlsAndRoutes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
